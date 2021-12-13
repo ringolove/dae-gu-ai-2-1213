ae0df5fc5792e85f174b413cd5217ac3a28585e8
ringolove <socialmusicwriter@gmail.mail>
2021-12-13 20:55:48 +0900

    sqlite3로 db 다루기

---
+++ init.py
@@ -0,0 +1,12 @@
+import sqlite3
+connect = sqlite3.connect('data.db')
+cursor = connect.cursor()
+cursor.execute('''
+    CREATE TABLE topics (
+        id        INTEGER      PRIMARY KEY AUTOINCREMENT,
+        title     VARCHAR (50) NOT NULL,
+        body      TEXT
+    )
+''')
+cursor.close()
+connect.close()
